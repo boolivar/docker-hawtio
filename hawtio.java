e817545c46e7ac6f29e66bbe366f55fd12e94d2d
Aleksey Krichevskiy <boolivar@gmail.com>
2025-02-23 23:36:52 +0200

    fix: hawtio version (#13)
    * feat: hawtio.java jbang script
    
    * feat: HAWTIO_VERSION build arg check

---
+++ hawtio.java
@@ -0,0 +1,9 @@
+//DEPS io.hawt:hawtio-embedded:${env.HAWTIO_VERSION}
+import io.hawt.embedded.Main;
+
+public class hawtio {
+
+    public static void main(String[] args) {
+        Main.run(args);
+    }
+}
